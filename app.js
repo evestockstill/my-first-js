14865a4725457fda4b790d7962461a22f2b57e0e
Eve Stockstill <evestockstillpdx@gmail.com>
2019-07-10 20:05:11 -0700

    my first js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const result = document.getElementById('result');
+const nameInput = document.getElementById('name');
+
+
+// eslint-disable-next-line no-unused-vars
+function updateResult() { 
+    result.textContent = nameInput.value;
+}
